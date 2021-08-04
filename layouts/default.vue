1edc34a4575234744f0dcebe1efeec1bf13e12a5
Felipe Costa <fcosta@unicampo.coop.br>
2021-08-04 20:12:49 -0300

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      fixed: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Planilha Fomulário',
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-row class="d-flex justify-center align-center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Bem-vindo ao sistema que transforma um arquivo excel em um CRUD
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Para começar precisamos de um arquivo de excel, podendo ele conter
+            informações ou não.
+          </p>
+          <p>
+            Arraste o arquivo ou clique no campo a baixo para selecioanr o
+            arquivo.
+          </p>
+          <hr class="my-3" />
+        </v-card-text>
+        <v-card-actions>
+          <v-row align="center" no-gutters>
+            <v-file-input
+              v-if="!jaCarregado"
+              v-model="arquivo"
+              label="File input"
+              outlined
+              dense
+              @change="uploadArquivo"
+            ></v-file-input>
+            <v-btn v-if="jaCarregado" block @click="jaCarregado = false"
+              >Avançar</v-btn
+            >
+          </v-row>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jaCarregado: false,
+      arquivo: null,
+    }
+  },
+  methods: {
+    uploadArquivo() {
+      console.log(this.arquivo)
+
+      if (this.arquivo !== null) this.jaCarregado = true
+    },
+  },
+}
+</script>
